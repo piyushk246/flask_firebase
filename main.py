36b77fb5e6eabaffaf671df4ab3035a89f04d02b
piyushk246 <65733681+piyushk246@users.noreply.github.com>
2023-11-20 20:27:35 +0530

    Update main.py

--- main.py
+++ main.py
@@ -1,11 +1,14 @@
 from flask import Flask, render_template
-from flask_socketio import SocketIO, emit
+from matplotlib.figure import Figure
+from matplotlib.backends.backend_agg import FigureCanvasAgg
+import io
+import base64
 from firebase_admin import credentials, initialize_app, db
 import threading
 import time
+import schedule
 
 app = Flask(__name__)
-socketio = SocketIO(app)
 
 # Initialize Firebase
 cred = credentials.Certificate("cred.json")
@@ -15,45 +18,64 @@ initialize_app(cred, {'databaseURL': "https://mitapp-8fd20-default-rtdb.firebase
 ref = db.reference('/')
 
 # Initialize empty lists for x and y data
-x_data = {key: [] for key in ['voltageData', 'currentData', 'TemperatureData', 'SoCData', 'SoPData', 'SoFData', 'SoHData']}
-y_data = {key: [] for key in ['voltageData', 'currentData', 'TemperatureData', 'SoCData', 'SoPData', 'SoFData', 'SoHData']}
+data = {key: {'x': [], 'y': []} for key in ['voltageData', 'currentData', 'TemperatureData', 'SoCData', 'SoPData', 'SoFData', 'SoHData']}
 
-def update_plot(value):
-    print(f"Received event: Value='{value}'")
-    
+# Create a Matplotlib Figure and Axis
+fig = Figure(figsize=(8, 6), dpi=100)
+ax = fig.add_subplot(111)
+
+# Create a FigureCanvasAgg to render the figure
+canvas = FigureCanvasAgg(fig)
+
+def update_plot():
     try:
+        value = ref.get()
         for key, values in value.items():
             x_value = list(range(len(values)))
             y_value = values
 
-            x_data[key] = x_value
-            y_data[key] = y_value
+            data[key]['x'] = x_value
+            data[key]['y'] = y_value
 
-            # Emit the new data to connected clients
-            socketio.emit('update_plot', {'x_data': x_data, 'y_data': y_data})
-            
+        # Clear the existing plot and redraw it with new data
+        ax.clear()
+        for key in data.keys():
+            ax.plot(data[key]['x'], data[key]['y'], label=key)
+        ax.legend()
+
+        # Draw the figure on the canvas
+        canvas.draw()
     except (ValueError, TypeError) as e:
         print(f"Error in update_plot: {e}")
     except Exception as e:
         print(f"Error in update_plot: {e}")
 
-def firebase_listener():
+def job():
+    with app.app_context():
+        update_plot()
+
+# Schedule the job to run every 5 seconds
+schedule.every(5).seconds.do(job)
+
+def run_schedule():
     while True:
-        value = ref.get()
-        if value:
-            update_plot(value)
-        time.sleep(5)
+        schedule.run_pending()
+        time.sleep(1)
 
-# Start the Firebase listener in a separate thread
-firebase_thread = threading.Thread(target=firebase_listener)
-firebase_thread.start()
+# Start the schedule in a separate thread
+schedule_thread = threading.Thread(target=run_schedule)
+schedule_thread.start()
 
-# Routes for Flask
 @app.route('/')
 def index():
-    return render_template('index.html')
+    # Convert the plot to a PNG image
+    output = io.BytesIO()
+    canvas.print_png(output)
+    img_data = base64.b64encode(output.getvalue()).decode('utf-8')
 
-# Start the Flask app with SocketIO support
+    print("Rendering template")
+    return render_template('index.html', img_data=img_data)
+schedule.every(5).seconds.do(job)
+
 if __name__ == '__main__':
-    socketio.run(app, host='0.0.0.0', port=5000)
-    
+    app.run(host='0.0.0.0', port=5000, debug=True)
